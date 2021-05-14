c1227c11409904ae5ebd0d40b88be6b615a5cf83
pgomezboza <pgomezboza@gmail.com>
2021-05-14 18:21:50 -0500

    update

---
+++ nop.c
@@ -0,0 +1,14 @@
+#include "monty.h"
+
+/**
+ * nop - no operation performed.
+ * @stack: double pointer to head of stack.
+ * @line_number: line number of current operation.
+ *
+ * Return: void
+ */
+void nop(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+	(void)line_number;
+}
