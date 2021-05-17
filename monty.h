3c26c5d86b5eb82bdf2622a1b51e85629ef2a7b8
adrs condezo <ra.condezomonge@gmail.com>
2021-05-16 22:48:44 -0500

    update monty header

--- monty.h
+++ monty.h
@@ -78,6 +78,7 @@ void swap(stack_t **stack, unsigned int line_number);
 void add(stack_t **stack, unsigned int line_number);
 void nop(stack_t **stack, unsigned int line_number);
 void sub(stack_t **stack, unsigned int line_number);
+void _div(stack_t **stack, unsigned int line_number);
 void mul(stack_t **stack, unsigned int line_number);
 void mod(stack_t **stack, unsigned int line_number);
 void free_stack(int status, void *arg);
