dad0e8695ab247abf056139d39b65a4fe0a6aace
adrs condezo <ra.condezomonge@gmail.com>
2021-05-16 22:47:33 -0500

    Add div function

---
+++ div.c
@@ -0,0 +1,33 @@
+#include "monty.h"
+
+/**
+ * mul - divdes top two elements of stack and push result
+ * @stack: double pointer to head of stack
+ * @line_number: line number of current operation
+ *
+ * Return: void
+ */
+void _div(stack_t **stack, unsigned int line_number)
+{
+	int n;
+
+	if (var.stack_len < 2)
+	{
+		dprintf(STDERR_FILENO,
+			"L%u: can't div, stack too short\n",
+			line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	if ((*stack)->n == 0)
+	{
+		dprintf(STDERR_FILENO,
+			"L%u: division by zero\n",
+			line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	n = (*stack)->n;
+	pop(stack, line_number);
+	(*stack)->n /= n;
+}
