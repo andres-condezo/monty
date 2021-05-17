d7263f174f275e9e4b31b01fbda8e3947ea48a20
adrs condezo <ra.condezomonge@gmail.com>
2021-05-16 23:19:18 -0500

    Fix typo

--- div.c
+++ div.c
@@ -1,7 +1,7 @@
 #include "monty.h"
 
 /**
- * div - divdes top two elements of stack and push result
+ * _div - divdes top two elements of stack and push result
  * @stack: double pointer to head of stack
  * @line_number: line number of current operation
  *
